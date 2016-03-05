5400f957cb0443f51343ca7ddd328751dc1ef8a2
nimo <nimmortalz@gmail.com>
2016-03-05 23:20:12 +0200

    bootstrap form

--- app/controllers/AuthConfigImpl.scala
+++ app/controllers/AuthConfigImpl.scala
@@ -11,78 +11,29 @@ import scala.reflect.{ClassTag, classTag}
 
 trait AuthConfigImpl extends AuthConfig {
 
-  /**
-    * A type that is used to identify a user.
-    * `String`, `Int`, `Long` and so on.
-    */
   type Id = Long
-
-  /**
-    * A type that represents a user in your application.
-    * `User`, `Account` and so on.
-    */
   type User = Account
-
-  /**
-    * A type that is defined by every action for authorization.
-    * This sample uses the following trait:
-    *
-    * sealed trait Role
-    * case object Administrator extends Role
-    * case object NormalUser extends Role
-    */
   type Authority = Role
 
-  /**
-    * A `ClassTag` is used to retrieve an id from the Cache API.
-    * Use something like this:
-    */
   val idTag: ClassTag[Id] = classTag[Id]
-
-  /**
-    * The session timeout in seconds
-    */
   val sessionTimeoutInSeconds: Int = 3600
-
-  /**
-    * A function that returns a `User` object from an `Id`.
-    * You can alter the procedure to suit your application.
-    */
   def resolveUser(id: Id)(implicit ctx: ExecutionContext): Future[Option[User]] = UserDAO.findUser(id)
 
-  /**
-    * Where to redirect the user after a successful login.
-    */
   def loginSucceeded(request: RequestHeader)(implicit ctx: ExecutionContext): Future[Result] = {
-    //    Future.successful(Redirect(routes.Messages.main()))
     val uri = request.session.get("access_uri").getOrElse(routes.Tasks.allTasks().url.toString)
     Future.successful(Redirect(uri).withSession(request.session - "access_uri"))
   }
 
-  /**
-    * Where to redirect the user after logging out
-    */
   def logoutSucceeded(request: RequestHeader)(implicit ctx: ExecutionContext): Future[Result] =
-    Future.successful(Redirect(routes.Sessions.login()))
+    Future.successful(Redirect(routes.Tasks.allTasks()))
 
-  /**
-    * If the user is not logged in and tries to access a protected resource then redirect them as follows:
-    */
   def authenticationFailed(request: RequestHeader)(implicit ctx: ExecutionContext): Future[Result] =
-//    Future.successful(Redirect(routes.Sessions.login()))
     Future.successful(Redirect(routes.Sessions.login()).withSession("access_uri" -> request.uri))
 
-  /**
-    * If authorization failed (usually incorrect password) redirect the user as follows:
-    */
   override def authorizationFailed(request: RequestHeader, user: User, authority: Option[Authority])(implicit context: ExecutionContext): Future[Result] = {
     Future.successful(Forbidden("no permission"))
   }
 
-  /**
-    * A function that determines what `Authority` a user has.
-    * You should alter this procedure to suit your application.
-    */
   def authorize(user: User, authority: Authority)(implicit ctx: ExecutionContext): Future[Boolean] = Future.successful {
     (user.role, authority) match {
       case ( Administrator, _ ) => true
@@ -91,16 +42,7 @@ trait AuthConfigImpl extends AuthConfig {
     }
   }
 
-  /**
-    * (Optional)
-    * You can custom SessionID Token handler.
-    * Default implementation use Cookie.
-    */
   override lazy val tokenAccessor = new CookieTokenAccessor(
-    /*
-     * Whether use the secure option or not use it in the cookie.
-     * Following code is default.
-     */
     cookieSecureOption = play.api.Play.isProd(play.api.Play.current),
     cookieMaxAge       = Some(sessionTimeoutInSeconds)
   )
